47353fb2564f820ce44425ea60d26d9e34b8546e
harantsuki <anyazosen@gmail.com>
2023-12-20 19:56:07 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const wp = document.querySelector('.wallpaper')
+const img = document.querySelector('.img-box')
+
+// img.addEventListener('click', function(){
+//     alert('halo');
+// })
+
+function sayang1() {
+    wp.classList.toggle('sevilan-1');
+}
+function sayang2() {
+    wp.classList.toggle('sevilan-2');
+}
+function sayang3() {
+    wp.classList.toggle('sevilan-3');
+}
